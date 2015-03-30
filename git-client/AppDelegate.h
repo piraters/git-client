aeea23917e69442654209205133883af2c58137b
shine <shine@shine.local>
2015-03-30 21:37:03 +0800

    Initial Commit

---
+++ git-client/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git-client
+//
+//  Created by shine on 15-3-30.
+//  Copyright (c) 2015年 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git-client/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git-client
+//
+//  Created by shine on 15-3-30.
+//  Copyright (c) 2015年 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
